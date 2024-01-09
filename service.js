4204d3ead5227cca4899278393e9b9bf1e4c709c
spenc688 <113399926+spenc688@users.noreply.github.com>
2024-01-09 14:48:46 -0600

    Update service.js

--- service.js
+++ service.js
@@ -1,8 +1,8 @@
 import { Service } from 'node-windows'
 
 const svc = new Service({
-    name: 'NZXT Starfield Compass Web Integration',
-    description: 'Web server for the Starfield compass web integration',
+    name: 'Saltburn Oliver Quick Web Integration',
+    description: 'Web server for the Saltburn Oliver Quick web integration',
     script: './index.js'
 })
 
